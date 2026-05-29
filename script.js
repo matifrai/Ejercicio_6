c63d30bf00ab360ce88331418a3b9e24092d6513
MATIAS ALBERTO FRAIMAN <49979893@ort>
2026-05-29 09:02:19 -0300

    falta bastante javaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputNombre = document.getElementById("NombreViajero");
+const inputEdad = document.getElementById("Edad");
+const inputPresupuesto = document.getElementById("Presupuesto");
+const inputDestino = document.getElementById("Destino");
+const inputTemporada = document.getElementById("Temporada");
+const inputSeguro = document.getElementById("Acepto");
+
